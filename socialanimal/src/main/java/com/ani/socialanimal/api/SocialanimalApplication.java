d8415a88442cbb4faf602d055bd9036d17ae2832
AnirudhMohanKumar <anirudhmkumar@gmail.com>
2018-12-16 15:37:01 +0530

    First commit

---
+++ socialanimal/src/main/java/com/ani/socialanimal/api/SocialanimalApplication.java
@@ -0,0 +1,14 @@
+package com.ani.socialanimal.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialanimalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialanimalApplication.class, args);
+	}
+
+}
+
